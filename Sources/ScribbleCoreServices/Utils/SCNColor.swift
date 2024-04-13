280deb99d0ef750136d6c55ebf65bf9a9088c54d
N3v1 <n3v1010@gmail.com>
2024-04-13 22:18:27 +0200

    [Chore]: Added CMYK and LAB to SCNColor

--- Sources/ScribbleCoreServices/Utils/SCNColor.swift
+++ Sources/ScribbleCoreServices/Utils/SCNColor.swift
@@ -9,7 +9,7 @@ import SwiftUI
 import Foundation
 
 @available(iOS 17.0, *)
-struct SCNColor: Equatable {
+@frozen public struct SCNColor: Equatable {
     let color: Color
 
     init(_ color: Color) {
@@ -45,16 +45,40 @@ struct SCNColor: Equatable {
 
         self.color = Color(red: red / 255.0, green: green / 255.0, blue: blue / 255.0)
     }
+    
+    init(hue: Double, saturation: Double, lightness: Double) {
+        self.color = Color(hue: hue, saturation: saturation, brightness: lightness)
+    }
+    
+    init(cyan: Double, magenta: Double, yellow: Double, black: Double) {
+        let red = (1.0 - cyan) * (1.0 - black)
+        let green = (1.0 - magenta) * (1.0 - black)
+        let blue = (1.0 - yellow) * (1.0 - black)
+        
+        self.color = Color(red: red, green: green, blue: blue)
+    }
+    
+    init(luminance: Double, a: Double, b: Double) {
+        let y = (luminance + 16.0) / 116.0
+        let x = a / 500.0 + y
+        let z = y - b / 200.0
+        
+        let r = x > 0.206893034 ? pow(x, 3) : (x - 16.0 / 116.0) / 7.787
+        let g = y > 0.206893034 ? pow(y, 3) : (y - 16.0 / 116.0) / 7.787
+        let b = z > 0.206893034 ? pow(z, 3) : (z - 16.0 / 116.0) / 7.787
+        
+        self.color = Color(red: r, green: g, blue: b)
+    }
 }
 
 @available(iOS 17.0, *)
-extension SCNColor {
+public extension SCNColor {
     static let primary = SCNColor(.primary)
     static let secondary = SCNColor(.secondary)
 }
 
 @available(iOS 17.0, *)
-extension Color {
+public extension Color {
     init(_ scnColor: SCNColor) {
         self.init(scnColor.color)
     }
