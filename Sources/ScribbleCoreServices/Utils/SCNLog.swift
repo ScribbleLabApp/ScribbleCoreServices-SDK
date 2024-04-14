b6a8cfd4aef01150cbc132723cbaa4c072f7621b
N3v1 <n3v1010@gmail.com>
2024-04-14 09:08:33 +0200

    [Chore]: Fix internal issue

--- Sources/ScribbleCoreServices/Utils/SCNLog.swift
+++ Sources/ScribbleCoreServices/Utils/SCNLog.swift
@@ -17,17 +17,17 @@ import Foundation
 public struct SCNLog {
     
     /// The subsystem to categorize log messages.
-    let subsystem: String
+    public let subsystem: String
     
     /// The category within the subsystem to further categorize log messages.
-    var category: String
+    public var category: String
     
     /// Initializes a new `SCNLog` instance with the provided subsystem and category.
     ///
     /// - Parameters:
     ///   - subsystem: The subsystem to categorize log messages. Typically, you'd use the bundle identifier of your app.
     ///   - category: The category within the subsystem to further categorize log messages.
-    init(subsystem: String, category: String) {
+    public init(subsystem: String, category: String) {
         self.subsystem = subsystem
         self.category = category
     }
@@ -35,7 +35,7 @@ public struct SCNLog {
     /// Accesses the logger associated with this `SCNLog` instance.
     ///
     /// - Returns: A `Logger` instance configured with the specified subsystem and category.
-    var logger: Logger {
+    public var logger: Logger {
         Logger(subsystem: subsystem, category: category)
     }
 }
@@ -60,7 +60,7 @@ public extension View {
     ///
     /// - Parameter closure: The block of code to be logged.
     /// - Returns: A view that logs the provided closure when executed.
-    func scnLog(_ closure: () -> Void) -> some View {
+    public func scnLog(_ closure: () -> Void) -> some View {
         closure()
         
         return self
