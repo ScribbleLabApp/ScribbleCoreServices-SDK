ec944987c19c0b15ae35c8fa059aa6047cefd308
N3v1 <n3v1010@gmail.com>
2024-04-21 15:06:09 +0200

    (fix): Change Xcode version in CI-Workflow

--- Package.swift
+++ Package.swift
@@ -18,17 +18,13 @@ let package = Package(
         .library(name: "ScribbleCoreServicesObjc", targets: ["ScribbleCoreServicesObjc"])
     ],
     dependencies: [
-        .package(url: "https://github.com/firebase/firebase-ios-sdk.git", from: "10.24.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
             name: "ScribbleCoreServices",
-            dependencies: [
-                .product(name: "FirebaseFirestore", package: "firebase-ios-sdk"),
-                .product(name: "FirebaseAuth", package: "firebase-ios-sdk")
-            ],
+            dependencies: [],
             path: "Sources/ScribbleCoreServices",
             publicHeadersPath: ".",
             cSettings: [
